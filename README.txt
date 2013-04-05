0615299154ce0710662c35d598aa01d67752f83d
Aaron Melton <aaron@aaronmelton.com>
2013-04-05 13:13:14 -0400

    Initial upload to GitHub

---
+++ README.txt
@@ -0,0 +1,8 @@
+SimpleExscriptExample.py
+
+DESCRIPTION:
+SimpleExscriptExample.py is a small big of Python code I wrote to test the use
+of the Exscript module.  See comments in the code for basic funcationality.
+
+DEPENDENCIES:
+Exscript module; https://github.com/knipknap/exscript/
---
+++ SimpleExscriptExample.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import Exscript
+
+from Exscript.util.interact	import read_login
+from Exscript.protocols		import SSH2
+
+account = read_login()				# Prompt the user for his name and password
+socket = SSH2()						# Set conenction type to SSH2
+socket.connect('192.168.1.1')		# Open connection to router
+socket.login(account)				# Authenticate on the remote host
+
+socket.execute('terminal length 0')	# Disable page breaks in router output
+									# socket.autoinit() doesn't seem to disable
+									# page breaks; Using standard command instead
+socket.execute('show run')			# Send command to router
+print socket.response				# Print results of command to screen
+
+socket.send('exit\r')				# Send command to exit gracefully
+									# socket.execute('exit') produces EOF error
+socket.close()						# Close connection
